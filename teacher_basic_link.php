d9f456b8d5d4c2f190e9fa3ade85d9d828f4e0ff
James Schneider <jschneider@illuminateed.com>
2019-02-04 15:44:24 -0800

    Teacher/instructor basic launch Ticket: Purpose of Change: Description of Change: Tags:
    Conflicts Resolved:

---
+++ teacher_basic_link.php
@@ -0,0 +1,21 @@
+<?php
+
+session_save_path('./');
+session_start();
+
+require_once('display.php');
+require_once('Loader.php');
+
+require_once('teacher_basic_link_template.php');
+
+$form_params = Loader::loadFormParams();
+
+$params = [
+    'form_params' => $form_params,
+];
+
+$template = new TeacherBasicLinkTemplate($params);
+$display = new Display();
+$display->add($template->toHtml());
+
+echo $display->toHtml();
