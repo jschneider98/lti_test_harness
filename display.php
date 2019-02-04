15f312780187d1845945ca1e2b5ba37362ca1f26
James Schneider <jschneider@illuminateed.com>
2019-02-04 15:41:58 -0800

    Simple display object Ticket: Purpose of Change: Description of Change: Tags:
    Conflicts Resolved:

---
+++ display.php
@@ -0,0 +1,41 @@
+<?php
+
+class Display
+{
+    public $content = [];
+
+    /**
+     *
+     */
+    public function add($var)
+    {
+        $this->content[] = $var;
+    }
+
+    /**
+     *
+     */
+    public function toHtml()
+    {
+        $html = <<< HTML
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+<a href="index.php">Config</a> | 
+<a href="teacher_deep_link.php">Teacher Deep Link</a> | 
+<a href="student_deep_link.php">Student Deep Link</a> |
+<a href="teacher_basic_link.php">Teacher Basic Link</a> |
+<a href="student_basic_link.php">Student Basic Link</a>
+<hr>
+
+HTML;
+       
+    $html .= implode("\n", $this->content);
+    $html .= <<< HTML
+
+</body>
+</html>
+HTML;
+        return $html;
+    }
+}
