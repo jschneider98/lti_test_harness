9e8627fffe4edfb62d48528b1756feacb7bab87e
James Schneider <jschneider@illuminateed.com>
2019-02-06 11:14:31 -0800

    New sso template Ticket: Purpose of Change: Description of Change: Tags:
    Conflicts Resolved:

---
+++ sso_template.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once('Loader.php');
+
+class SsoTemplate
+{
+    public $url;
+    public $params = [];
+
+    /**
+     *
+     */
+    public function __construct($params = [])
+    {
+        $form_params = [];
+
+        extract($params, EXTR_IF_EXISTS);
+
+        $this->params = $form_params;
+        $this->url = $form_params['custom_sso_url'];
+
+        $client = Loader::loadClient($form_params);
+
+        $oauth = $client->getOauth($this->url, 'POST', $form_params);
+        $this->params = $oauth->get_parameters();
+    }
+
+    /**
+     *
+     */
+    public function set($key, $value)
+    {
+        $this->params[$key] = $value;
+    }
+
+    /**
+     *
+     */
+    public function toHtml()
+    {
+        extract($this->params);
+
+        $html = <<< HTML
+<h2>SSO Launch</h2>
+<hr>
+<h5>Email: {$lis_person_contact_email_primary}</h5>
+<hr>
+<form
+    action="{$this->url}"
+    name="ltiLaunchForm"
+    id="ltiLaunchForm"
+    method="post"
+    enctype="application/x-www-form-urlencoded"
+    target="_blank"
+>
+HTML;
+
+    foreach ($this->params as $key => $val) {
+        if ($key != 'submit_lti_launch') {
+        $html .= <<< HTML
+<b>$key:</b> $val
+<hr>
+<input type="hidden" name="$key" value="{$val}">
+HTML;
+        }
+    }
+
+    $html .= <<< HTML
+
+    <input type="submit" name="submit_lti_launch" value="LTI Launch">
+
+</form>
+HTML;
+
+        return $html;
+    }
+}
