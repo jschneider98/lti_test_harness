8556a4a5fffe36d930e3a321ce8eaa8d3a0106de
James Schneider <jschneider@illuminateed.com>
2019-02-05 16:07:01 -0800

    Added outcome params Ticket: Purpose of Change: Description of Change: Tags:
    Conflicts Resolved:

--- Loader.php
+++ Loader.php
@@ -30,6 +30,8 @@ class Loader
             'ext_content_return_types'         => self::loadParam('ext_content_return_types', 'lti_launch_url'),
             'ext_content_return_url'           => self::loadParam('ext_content_return_url', 'http://localhost:8081/deep_link_return.php'),
             'lis_person_contact_email_primary' => $lis_person_contact_email_primary,
+            'lis_outcome_service_url'          => self::loadParam('lis_outcome_service_url', 'http://localhost:8081/score_add.php'),
+            'lis_result_sourcedid'             => self::loadParam('lis_result_sourcedid', 1),
             'lti_message_type'                 => self::loadParam('lti_message_type', 'basic-lti-launch-request'),
             'lti_version'                      => self::loadParam('lti_version', 'LTI-1p0'),
             'resource_link_id'                 => self::loadParam('resource_link_id', 1),
